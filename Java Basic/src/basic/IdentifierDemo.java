cbdb19871a53675a3f6e38034064d986c8e3b24d
dypatil <dypatil@pc>
2025-06-30 11:50:57 +0530

    Day 2

---
+++ Java Basic/src/basic/IdentifierDemo.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		
+		int $num1 = 20;
+		
+		String StudentName = "Heti";
+		
+		int FOR =12;
+		
+		System.out.println("Values of the number variables is "+ $num1);
+
+	}
+
+}
