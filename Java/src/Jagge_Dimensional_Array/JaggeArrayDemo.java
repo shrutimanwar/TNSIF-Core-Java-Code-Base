caa882ccc131b725950eed6344233ff21c2ebbd6
ADMIN <ADMIN@DESKTOP-8R2Q0EA.mshome.net>
2025-07-09 20:48:25 +0530

    Day 10

---
+++ Java/src/Jagge_Dimensional_Array/JaggeArrayDemo.java
@@ -0,0 +1,34 @@
+package Jagge_Dimensional_Array;
+
+
+class JaggedArray
+{
+	static void printArray(int c[][]) {
+		System.out.println("Array of elements are as follows");
+		for (int i=0; i<=c.length;i++)
+		{
+			for(int j=0; j<c[i].length;j++)
+			{
+				System.out.print(c[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+public class JaggeArrayDemo {
+
+	public static void main(String[] args) {
+		
+	int c[][]= { {1,2,3,12,34} , {10,20} , {1} , {1,2,78,87} };
+		
+		System.out.println("No. of row in C array :"+c.length);
+JaggedArray.printArray(c);
+
+
+System.out.println("gfdcgfdghdcgh");
+
+	}
+
+}
